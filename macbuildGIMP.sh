320d2f02cd5961e6c2e9bf129c7b849947e97b0d
Benitoite <kd6kxr@gmail.com>
2020-05-12 11:19:54 -0700

    Update macbuildGIMP.sh

--- macbuildGIMP.sh
+++ macbuildGIMP.sh
@@ -53,7 +53,7 @@ git clone https://github.com/freedesktop/poppler.git
 git clone https://github.com/uclouvain/openjpeg.git
 git clone https://anongit.freedesktop.org/git/poppler/poppler-data.git
 git clone https://github.com/GNOME/gjs.git
-git clone https://github.com/mozilla/gecko-dev.git
+git clone https://github.com/ptomato/mozjs.git
 
 curl http://ftp.gnu.org/gnu/autogen/rel5.18.16/autogen-5.18.16.tar.xz -o autogen.xz && tar xf autogen.xz && rm autogen.xz && mv autogen-5* autogen
 curl https://gmplib.org/download/gmp/gmp-6.2.0.tar.xz -o gmp.xz && tar xf gmp.xz && rm gmp.xz && mv gmp-6* gmp
@@ -161,7 +161,10 @@ cd ~/poppler && mkdir build && cd build && DYLIB_CURRENT_VERSION=8.0.1 DYLIB_COM
 
 cd ~/poppler-data && mkdir build && cd build && DYLIB_CURRENT_VERSION=8.0.1 DYLIB_COMPATIBILITY_VERSION=7.0.0 cmake .. -DCMAKE_OSX_SYSROOT:PATH=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk -DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.9 -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/opt/local -DICONV_LIBRARIES=/opt/local/lib/libiconv.dylib && make -j8 && sudo make install
 
-cd ~/gecko-dev/js/src && 
+# NOTE: mozjs68 requires OSX 10.11 minimum
+cd ~/mozjs/intl/icu/source && mkdir obj && cd obj && CC=clang CXX=clang++ CFLAGS="-arch x86_64 -mmacosx-version-min=10.11 -I/opt/local/include" LDFLAGS="-arch x86_64 -mmacosx-version-min=10.9 -L/opt/local/lib -Wl,-rpath -Wl,/opt/local/lib" CPPFLAGS="-arch x86_64 -mmacosx-version-min=10.11 -I/opt/local/include" AUTOCONF=autoconf PYTHON=python PKG_CONFIG_PATH=/opt/local/lib/pkgconfig APPSTREAM_GLIB_LIBS="-lappstream-glib -L/opt/local/lib" APPSTREAM_GLIB_CFLAGS="-I/opt/local/include" ../configure  --prefix=/opt/local --with-macos-sdk=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk --disable-xcode-checks --with-libclang-path=/Library/Developer/CommandLineTools/usr/lib --with-clang-path=/usr/bin/clang --disable-samples --disable-tests && make PKGDATA_TRAILING_SPACE= && make -j8 && touch ../../LICENSE && sudo make install
+
+cd ~/mozjs/js/src && mkdir obj && cd obj && CC=clang CXX=clang++ CFLAGS="-arch x86_64 -mmacosx-version-min=10.11 -I/opt/local/include" LDFLAGS="-arch x86_64 -mmacosx-version-min=10.11 -L/opt/local/lib -Wl,-rpath -Wl,/opt/local/lib" CPPFLAGS="-arch x86_64 -mmacosx-version-min=10.9 -I/opt/local/include" AUTOCONF=autoconf PYTHON=python PKG_CONFIG_PATH=/opt/local/lib/pkgconfig APPSTREAM_GLIB_LIBS="-lappstream-glib -L/opt/local/lib" APPSTREAM_GLIB_CFLAGS="-I/opt/local/include" ../configure  --prefix=/opt/local --with-macos-sdk=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk --disable-xcode-checks --with-libclang-path=/Library/Developer/CommandLineTools/usr/lib --with-clang-path=/usr/bin/clang && make -j8 && sudo make install
 
 cd ~/gjs && LD=ld CC=/usr/bin/clang CXX=/usr/bin/clang++ LIBRARY_PATH=/opt/local/lib meson setup --cross-file=~/maccross  --prefix=/opt/local --buildtype=release --layout=mirror --default-library=both -D instrospection=false _build . && ninja -C _build && sudo ninja -C _build install
 
